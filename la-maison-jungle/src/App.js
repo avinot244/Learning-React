5cbcd6bdc42442082129bdac8b87d6814dbe9fa7
avionot244 <aymericvinot38@gmail.com>
2024-02-20 16:12:25 +0100

    Adding conditional behavior

--- la-maison-jungle/src/App.js
+++ la-maison-jungle/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/App.css';
 import Banner from './Components/Banner'
 import Cart from "./Components/Cart"
+import ShoppingList from './Components/ShoppingList';
 
 function App() {
   return (
     <React.StrictMode>
       <Banner />
-      <Cart />
+      {/* <Cart /> */}
+      <ShoppingList />
     </React.StrictMode>
   );
 }
--- la-maison-jungle/src/Components/Banner.js
+++ la-maison-jungle/src/Components/Banner.js
@@ -2,11 +2,15 @@ import "../styles/Banner.css"
 import logo from "../assets/logo.png"
 
 function Banner(){
+    const title = "La maison jungle"
+
     return (
-    <div className="lmj-banner">
-        <img src={logo} alt="La maison jungle" className="lmj-logo"/>
-        <h1 className="lmg-title">La maison jungle</h1>
-    </div>
+        <div className="lmj-banner">
+            <div className="lmj-banner-row">
+                <img src={logo} alt="La maison jungle" className="lmj-logo"/>
+                <h1 className="lmg-title">{title}</h1>
+            </div>
+        </div>
     );
 }
 
